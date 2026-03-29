e84ba4a6497943507245113366c0f8c58ef25ec7
Morrolinux <morrolinux@gmail.com>
2026-03-29 22:49:45 +0200

    add nautilus-open-any-terminal extension

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,6 +10,9 @@ dnf -y install libvirt virt-manager qemu-kvm flatpak-builder wlr-randr
 
 # User apps
 dnf -y install nautilus kitty mpv obs-studio gnome-terminal
+curl -Lo /etc/yum.repos.d/nautilus-open-any-terminal.repo \
+  https://copr.fedorainfracloud.org/coprs/monkeygold/nautilus-open-any-terminal/repo/fedora-43/monkeygold-nautilus-open-any-terminal-fedora-43.repo
+dnf install -y nautilus-open-any-terminal
 
 #### Enable podman
 

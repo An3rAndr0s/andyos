defdd5127a9c394d77d1cdef4196a9a6e543f7fa
Morrolinux <morrolinux@gmail.com>
2026-03-29 22:14:12 +0200

    add essential utilities

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,59 +5,11 @@ set -ouex pipefail
 ## DNF5 Speedup
 sed -i '/^\[main\]/a max_parallel_downloads=10' /etc/dnf/dnf.conf
 
-#### Install Cachy kernel and utilities ####
-# create a shims to bypass kernel install triggering dracut/rpm-ostree
-# seems to be minimal impact, but allows progress on build
-cd /usr/lib/kernel/install.d \
-&& mv 05-rpmostree.install 05-rpmostree.install.bak \
-&& mv 50-dracut.install 50-dracut.install.bak \
-&& printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install \
-&& printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install \
-&& chmod +x  05-rpmostree.install 50-dracut.install
+## System apps
+dnf -y install libvirt virt-manager qemu-kvm flatpak-builder wlr-randr
 
-## Install CachyOS kernel
-dnf5 -y copr enable bieszczaders/kernel-cachyos
-dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --setopt=protected_packages= --setopt=protect_running_kernel=False
-rm -rf /lib/modules/* # Remove kernel files that remain
-dnf5 -y install kernel-cachyos kernel-cachyos-devel-matched --allowerasing
-
-dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
-rm -rf /usr/lib/systemd/coredump.conf
-dnf5 -y install cachyos-settings scx-scheds scx-tools-git scx-manager ananicy-cpp --allowerasing
-systemctl enable scx_loader.service
-
-## Experimental: use ADIOS IO scheduler by default on nvme disks
-echo > /etc/udev/rules.d/60-ioschedulers.rules << EOF
-ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", \
-    ATTR{queue/scheduler}="adios"
-EOF
-
-sudo mkdir -p /etc/scx_loader
-echo > /etc/scx_loader/config.toml << EOF
-default_sched = "scx_lavd"
-default_mode = "Auto"
-EOF
-
-# restore kernel install
-mv -f 05-rpmostree.install.bak 05-rpmostree.install \
-&& mv -f 50-dracut.install.bak 50-dracut.install
-cd -
-
-# Regen initramfs
-releasever=$(/usr/bin/rpm -E %fedora)
-basearch=$(/usr/bin/arch)
-KERNEL_VERSION=$(dnf list kernel-cachyos -q | awk '/kernel-cachyos/ {print $2}' | head -n 1 | cut -d'-' -f1)-cachyos1.fc${releasever}.${basearch}
-# Ensure Initramfs is generated
-depmod -a ${KERNEL_VERSION}
-mkdir -p /var/roothome
-export DRACUT_NO_XATTR=1
-/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add ostree -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
-chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
-
-
-#### Install COSMIC DESKTOP ####
-dnf5 -y copr enable ryanabx/cosmic-epoch
-dnf5 -y install cosmic-desktop
+# User apps
+dnf -y install nautilus kitty mpv obs-studio gnome-terminal
 
 #### Enable podman
 

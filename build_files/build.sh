5e112a2fadc0bf960b22306ef12205c1f26a96d6
Morrolinux <morrolinux@gmail.com>
2026-05-11 18:03:57 +0200

    set dank link as default

--- build_files/build.sh
+++ build_files/build.sh
@@ -36,18 +36,24 @@ dnf -y install noctalia-shell
 sudo curl --output-dir "/etc/yum.repos.d/" \
   --remote-name "https://copr.fedorainfracloud.org/coprs/avengemedia/dms/repo/fedora-$(rpm -E %fedora)/avengemedia-dms-fedora-$(rpm -E %fedora).repo"
 dnf -y install quickshell dms greetd dms-greeter --allowerasing 
-## Install greetd login manager with dank configuration (still needs some work)
-# mkdir -p /etc/greetd/
-# cat > /etc/greetd/config.toml << EOF
-# [terminal]
-# vt = 1
-# [default_session]
-# user = "greeter"
-# command = "dms-greeter --command niri"
-# EOF
-# rm /etc/systemd/system/display-manager.service
-# ln -s /usr/lib/systemd/system/greetd.service /etc/systemd/system/display-manager.service
-## ABILITARE LE NOTIFICHE: systemctl --user enable --now dms.service
+#
+# Install greetd login manager with dank configuration (still needs some work)
+mkdir -p /etc/greetd/
+cat > /etc/greetd/config.toml << EOF
+[terminal]
+vt = 1
+[default_session]
+user = "greeter"
+command = "dms-greeter --command niri"
+EOF
+rm /etc/systemd/system/display-manager.service
+ln -s /usr/lib/systemd/system/greetd.service /etc/systemd/system/display-manager.service
+systemctl enable --force greetd.service
+
+mkdir -p /etc/skel/.config/systemd/user/graphical-session.target.wants
+ln -s /usr/lib/systemd/user/dms.service /etc/skel/.config/systemd/user/graphical-session.target.wants/
+mkdir -p /etc/skel/.config/niri/
+cp -rf /ctx/dot_config/niri/config.kdl /etc/skel/.config/niri/
 
 # DEV packages
 # cargo evtest git input-remapper libevdev-devel libinput-utils python3-devel
